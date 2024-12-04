af275771b0867d1a0b2599f491cfcb8a3a5b85a2
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-12-03 22:30:10 -0500

    Add hacker.js
    Added Hacker playbook, filled out

---
+++ playbooks/posts/hacker.js
@@ -0,0 +1,141 @@
+export const hackerPlaybook = {
+  name: "Hacker",
+  type: "Post",
+  blurb:
+    "Brave souls who take blade and acid to the verdant sea, carving paths for their companions. Hackers thrive when armed with hewing and serrated weapons.",
+  edgesQS: ["Iron", "Instinct", "Teeth"],
+  skillsQS: [
+    "Concoct",
+    "Hack",
+    "Harvest",
+    "Hunt",
+    "Outwit",
+    "Sense",
+    "Wavewalk",
+  ],
+  languagesQS: ["Knock", "Signalling"],
+  resourcesQS: {
+    salvage: ["Wreck-Iron Plating", "Abandoned Scrap"],
+    specimens: ["Wilds Apricots", "Scorpions in a Jar"],
+    whispers: ["An Ironroot Fallen"],
+    charts: ["An Arboreal Diagram"],
+  },
+  drivesQS: ["Lay a mawship to rest", "Survive a leviathan plant encounter"],
+  miresQS: [
+    "Insects seek your skin",
+    "The waves call for you to travel deeper than necessary",
+  ],
+  aspects: [
+    {
+      name: "Scars Remember",
+      type: "Trait",
+      length: 5,
+      description: "The wildsea teaches harsh, but useful, lessons.",
+    },
+    {
+      name: "Trailblazer",
+      type: "Trait",
+      length: 3,
+      description:
+        "Increase impact when finding routes and clearing paths through vegetation.",
+    },
+    {
+      name: "Living Antidote",
+      type: "Trait",
+      length: 3,
+      description:
+        "Use a task to create a rare resource, 'Pure Spit' or 'Pure Sweat'.",
+    },
+    {
+      name: "Wild Harvest",
+      type: "Trait",
+      length: 3,
+      description:
+        "Treat conflicts as triumphs when attacking or defending against plant-type hazards.",
+    },
+    {
+      name: "Crezzercamo",
+      type: "Trait/Gear/Companion",
+      length: 3,
+      description:
+        "Mark to become undetectable to plant-type hazards and creatures for the duration of a scene.",
+    },
+    {
+      name: "Hard-Won Instincts",
+      type: "Trait",
+      length: 3,
+      description:
+        "You can tell which branches will break and which will hold at a glance. Increase your ship's Saws rating by one when you're aboard.",
+    },
+    {
+      name: "Petalcrusher",
+      type: "Trait",
+      length: 3,
+      description:
+        "You ignore mire caused by plant-based hazards and horros, and are resistant to acids, toxins, and hallucinatory effects caused by predatory plants.",
+    },
+    {
+      name: "Rusting-Leaf Ale",
+      type: "Gear",
+      length: 2,
+      description:
+        "Becomes more flavourful the more it gets jostled and shaken. Whenever you gain a benefit from a concoction or meal, increase its track by two boxes.",
+    },
+    {
+      name: "Grappling Chain",
+      type: "Gear",
+      length: 2,
+      description:
+        "Designed to find quick perchase in bark and boughs. Treat conflicts as triumphs when traversing the sea.",
+    },
+    {
+      name: "Chemical Censer",
+      type: "Gear",
+      length: 3,
+      description:
+        "Mark to create a small area of smoke that repels insects and lowers visibility.",
+    },
+    {
+      name: "Wavestrider Jacket",
+      type: "Gear",
+      length: 2,
+      description:
+        "Resist the hazardous effects of crezzerin exposure, insect bites, and poisons.",
+    },
+    {
+      name: "Jagserry",
+      type: "Gear",
+      length: 3,
+      description:
+        "The archetypal hacker weapon, a sawtooth sabre that deals CQ Hewing and Serrated damage.",
+    },
+    {
+      name: "Sawtooth Chakram",
+      type: "Gear",
+      length: 3,
+      description:
+        "A set of sharpened circular blades made for throwing, dealing LR Keen or Serrated damage.",
+    },
+    {
+      name: "Defoliant Grenades",
+      type: "Gear",
+      length: 3,
+      description:
+        "Explosive packets that burst when lobbed with force. Deals LR Acid or Salt damage.",
+    },
+    {
+      name: "Gyrewing Moths",
+      type: "Companion",
+      length: 5,
+      description:
+        "A trained swarm of ragged insects. Burn to neutralize the effects of bad air and airborne spores for your crew for the rest of a scene.",
+    },
+    {
+      name: "Prow-Nose Porcupine",
+      type: "Companion",
+      length: 2,
+      description:
+        "Not the easiest creature to pet, but you learn to live with it. Ignore cut caused by pain or discomfort.",
+    },
+  ],
+};
