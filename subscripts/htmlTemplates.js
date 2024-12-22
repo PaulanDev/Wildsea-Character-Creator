df40b9d5d92d2b473b71a8ae8175772329bf2a36
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-12-22 18:34:58 -0500

    Add htmlTemplates.js
    Moved button templates into this file, with some supporting functions

---
+++ subscripts/htmlTemplates.js
@@ -0,0 +1,50 @@
+const isEdgeSelectedElsewhere = (holder, check) => {
+  for (let pb in holder) {
+    if (holder[pb].edge == check) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const capitalize = (str) => {
+  return str[0].toUpperCase() + str.slice(1);
+};
+
+export const playbookBtnTemplate = (name, type, blurb) => {
+  return `<div id='${name}' class='${type}-choice-btn playbook-choice-btn'>
+        <h2 class='text-center'>
+        ${name}
+        </h2>
+        <p class='text-center'>
+        ${blurb}
+        </p></div>`;
+};
+
+export const edgeBtnTemplate = (edge, pb, sheet, db) => {
+  return `<div id='${pb}-edge-${edge}' class='${pb}-edge ${edge}-btn edges ${
+    isEdgeSelectedElsewhere(sheet, edge) ? "locked-edge" : ""
+  }'>
+      <h3>${edge}</h3>
+      <p>${capitalize(
+        db.find((entry) => entry.name == edge).tagline.slice(11)
+      )}</p>
+      </div>`;
+};
+
+export const skillBtnTemplate = (skill) => {
+  return `<div id='${skill}-skill-counter' class=' skill-counter'>
+      <div class="skill-name">${skill}</div>
+      <div class="skill-val-dec-btn">-</div>
+      <div class="${skill}-value skill-value" id='${skill}-points'>0</div>
+      <div class="skill-val-inc-btn">+</div>
+      </div>`;
+};
+
+export const aspectBtnTemplate = (aspect, type) => {
+  return `<div class='${type}-aspect'>
+          <h3>${aspect["name"]}</h3>
+          <p>${aspect["length"]} Track ${aspect["type"]}</p>
+          <p>${aspect["description"]}</p>
+          </div>`;
+};
