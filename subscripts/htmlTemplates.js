55cd0038a86d196d0d464b7575df137803f69f08
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-12-25 11:49:38 -0500

    Update htmlTemplates.js
    Added getSkillPointtoal function, using it to generate initial value on button load

--- subscripts/htmlTemplates.js
+++ subscripts/htmlTemplates.js
@@ -11,6 +11,16 @@ const capitalize = (str) => {
   return str[0].toUpperCase() + str.slice(1);
 };
 
+export const getSkillPointTotal = (skill, holder) => {
+  let totalPoints = 0;
+  for (let pb in holder) {
+    if (holder[pb].skills.hasOwnProperty(skill)) {
+      totalPoints += holder[pb].skills[skill];
+    }
+  }
+  return totalPoints;
+};
+
 export const playbookBtnTemplate = (name, type, blurb) => {
   return `<div id='${name}' class='${type}-choice-btn playbook-choice-btn'>
         <h2 class='text-center'>
@@ -32,11 +42,14 @@ export const edgeBtnTemplate = (edge, pb, sheet, db) => {
       </div>`;
 };
 
-export const skillBtnTemplate = (skill) => {
+export const skillBtnTemplate = (skill, holder) => {
   return `<div id='${skill}-skill-counter' class=' skill-counter'>
       <div class="skill-name">${skill}</div>
       <div class="skill-val-dec-btn">-</div>
-      <div class="${skill}-value skill-value" id='${skill}-points'>0</div>
+      <div class="${skill}-value skill-value" id='${skill}-points'>${getSkillPointTotal(
+    skill,
+    holder
+  )}</div>
       <div class="skill-val-inc-btn">+</div>
       </div>`;
 };
