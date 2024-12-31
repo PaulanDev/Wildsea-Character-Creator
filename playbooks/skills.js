9384b0bd0efc3bd45a4ed5a852b6b311f79d5ae7
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2024-12-31 11:53:49 -0500

    Add skills.js
    Added skills.js

---
+++ playbooks/skills.js
@@ -0,0 +1,268 @@
+export const SKILLS = [
+  {
+    name: "Brace",
+    blurb:
+      "A skill of defence, hard-headedness, and immobility. Use Brace to weather the ravages of weather, exhaustion, and those that want to see you fall.",
+    examples: [
+      "Clinging to life in the toughest situations",
+      "Shrugging off attempts to manipulate or confuse you",
+      "Blocking or parrying an incoming attack",
+      "Keeping your footing through a storm or rootquake",
+      "Shouldering you way through a crowd",
+    ],
+  },
+  {
+    name: "Break",
+    blurb:
+      "A skill of fragments, shockwaves, and hammer-blows. Use Break to smash through stone, metal, and machinery, working against the efforts of civilization.",
+    examples: [
+      "Reducing a building or obstacle to rubble",
+      "Charting a course through ruins and wreckage",
+      "Relying on hammers and fists in a skirmish",
+      "Assessing a structure's stability and weak points",
+      "Setting and disarming explosives",
+    ],
+  },
+  {
+    name: "Concoct",
+    blurb:
+      "A skill of reactions, essences, and deeper understanding. Use Concoct to collect or distill chemicals, and to unpick the mysteries surrounding the mutative potential of crezzerin.",
+    examples: [
+      "Creating and administering potions and poisons",
+      "Catalyzing unstable alchemical elements",
+      "Mulling hearty brews for healing or relaxation",
+      "Analyzing and altering potential ingredients",
+      "Parsing the essence of the crezzer-tinged",
+    ],
+  },
+  {
+    name: "Cook",
+    blurb:
+      "A skill of spices and speculation, meats and fruits and heat. Use Cook to prepare meals, brew herbal drinks or alcohol, and render speciments into their most valuable parts.",
+    examples: [
+      "Using speciments to create meals and beverages",
+      "Developing bolstering dishes to prepare for danger",
+      "Turning spices and seasoning into weapons of war",
+      "Handling heat sources safely in tense situations",
+      "Buthering, rendering, and storing specimens",
+    ],
+  },
+  {
+    name: "Delve",
+    blurb:
+      "A skill of dust, remnants, and forgotten ways. Use Delve to explore the ruins of the old world, traversing barriers and unearthing secrets.",
+    examples: [
+      "Abseiling down a collapsing passageway",
+      "Squeezing through narrow spaces and collapses",
+      "Cracking locks and disarming ancient traps",
+      "Scambling through wreckage and detritus at speed",
+      "Interpreting pre-verdant and artefacts",
+    ],
+  },
+  {
+    name: "Flourish",
+    blurb:
+      "A skill of showmanship, rhythm, and performance. Use Flourish to enchant, impress, or dazzle those around you, worming your way into their heart and words and deeds.",
+    examples: [
+      "Playing songs or telling stories with dramatic flair",
+      "Dressing or acting to stand out and be noticed",
+      "Moving with poise, flow, and balance",
+      "Creating art that stirs the soul",
+      "Inking skin with memories of triumph and loss",
+    ],
+  },
+  {
+    name: "Hack",
+    blurb:
+      "A skill of sweat, sap, and serrated blades. Use Hack to carve a path right through the wilderness of the rustling waves, imposing order on chaotic growth.",
+    examples: [
+      "Chopping or slicing your way through the wilds",
+      "Charting a course through snarled, confusing waves",
+      "Rushing into combat with axes, saws, and blades",
+      "Identifying the hazards of an unknown plant",
+      "Spinning tales of petalled leviathans and living rifts",
+    ],
+  },
+  {
+    name: "Harvest",
+    blurb:
+      "A skill of reaping, gleaning, and culling. Use Harvest to gather fruits, flowers, and other edibles from the wild waves, and to understand the flow and cycles of nature.",
+    examples: [
+      "Locating and collecting useful speciments",
+      "Identifying plants and their properties at a glance",
+      "Noticing breaks and oddities in the wilds around you",
+      "Understanding and exploiting natural phenomena",
+      "Sheparding spores, sporuts, and insects",
+    ],
+  },
+  {
+    name: "Hunt",
+    blurb:
+      "A skill of observation, marksmanship, and precise action. Use Hunt to turn the tables ont he sea's many predators, and pick the best of their trophies.",
+    examples: [
+      "Tracking effectively across hostile terrain",
+      "Lining up shots from afar in a fight",
+      "Reeling off information about beasts you've bested",
+      "Rending skills in useful speciments",
+      "Showing off trophies and hard-won scars",
+    ],
+  },
+  {
+    name: "Outwit",
+    blurb:
+      "A skill of shadows and secrets well-kept. Use Outwit to avoid threats, or to move them right yo where you want them.",
+    examples: [
+      "Moving quickly and quietly",
+      "Surprising opponents before melting away again",
+      "Infiltrating spaces that are definitely off limits",
+      "Disguising your voice, scent, or even your whole self",
+      "Talking smoothly and lying disarmingly",
+    ],
+  },
+  {
+    name: "Rattle",
+    blurb:
+      "A skill of medning, maintenance, and adaptation. Use Rattle to weave offments and salvage into workable equipment in a pinch, or to inker with gear and objects.",
+    examples: [
+      "Using salvage to repair damaged gear",
+      "Understanding schematics and blueprints",
+      "Spewing confusing jargon or incisive questions",
+      "Crafting temporary gear with odds and ends",
+      "Patching up a ship to keep it running 'til next port",
+    ],
+  },
+  {
+    name: "Scavenge",
+    blurb:
+      "A skill of searching, spit, and polish. Use Scavenge to root out valuable salvage from dross, to pick treasure from trash.",
+    examples: [
+      "Locating and collecting useful salvage",
+      "Weighing the worth of an object or trade",
+      "Using damaged mechanisms effectively",
+      "Turning the environment to your benefit in a brawl",
+      "Identifying the properties of refuse and oddities",
+    ],
+  },
+  {
+    name: "Sense",
+    blurb:
+      "A skill of glances, insight, and intuition. Use Sense to push your awareness of the world past its usual limits, both in terms of environmental awareness and social understanding.",
+    examples: [
+      "Honing one of your sense to gather information",
+      "Reacting to an unseen or unexpected attack",
+      "Detecting and defining nascent whispers",
+      "Becoming aware of attention focused your way",
+      "Emphasizing effectively with others",
+    ],
+  },
+  {
+    name: "Study",
+    blurb:
+      "A skill of meticulous recording and focused investigation. Use Study to learn the import of details others would miss, make connections, and sort information.",
+    examples: [
+      "Creating a chart as you travel a new area",
+      "Discovering evidence in the aftermath of an event",
+      "Slowly improving your knowledge of a language",
+      "Determining a pattern, flaw, or weakness",
+      "Gleaning information from cryptic notes",
+    ],
+  },
+  {
+    name: "Sway",
+    blurb:
+      "A skill of convincing, arguing, and capitulating with aplomb. Use Sway to convince others to act or hold back from acting, or to change hearts and minds to suit your purposes.",
+    examples: [
+      "Convincing others to change their minds on a topic",
+      "Making friends and influencing people",
+      "Taking the reins of a crowd's emotion",
+      "Bandying words while absorbing information",
+      "Threatening through inference",
+    ],
+  },
+  {
+    name: "Tend",
+    blurb:
+      "A skill of healing, sustenance, and recovery. Use Tend to heal injuries of the crew, nurture plants, and soothe the minds of both beasts and your fellow crewmates.",
+    examples: [
+      "Using speciments to heal an injured crewmate",
+      "Growing fruits, herbs, and spices",
+      "Calming and understanding the emotions of others",
+      "Devising and discussing new medical treatments",
+      "Communicating your intentions to beasts and birds",
+    ],
+  },
+  {
+    name: "Vault",
+    blurb:
+      "A skill of dizzying heights and impressive acrobatics. Use Vault to climb, leap, and tumble your way through an environment at speed.",
+    examples: [
+      "Scaling rigging, walls, or leviathan-hides",
+      "Landing safely after falling from a great height",
+      "Dodging away from incoming damage",
+      "Leaping from rooftop to rooftop during a chase",
+      "Controlling a glider as you soar through the skies",
+    ],
+  },
+  {
+    name: "Wavewalk",
+    blurb:
+      "A skill of seared soles, brachiation, and animalistic elegance. Use Wavewalk to personally navigae the wilds without the use of a ship, despite the danger.",
+    examples: [
+      "Leaping from branch to branch as they sway",
+      "Swinging on vines or grappling ropes",
+      "Passing through foliage without fear of crezzerin",
+      "Brachiating as fast as others can sprint",
+      "Discovering the secrets of the surrounding waves",
+    ],
+  },
+  {
+    name: "Chthonic",
+    blurb:
+      "An ancient human language, once split into thousands of unique dialects spoken by different groups of ardent survivors and now slowly merging into one true tongue again. Lore might include details of ancient human rituals and spirit-knowledge, as well as cultural information on the ardent.",
+  },
+  {
+    name: "Saprekk",
+    blurb:
+      "The thick, rolling tongue of the sktus, almost entirely unchanged despite hundreds of years of development on the wild waves. For non-ektus to have learned even a smattered of Saprekk is often seen as a great mark of respect. Lore might include tales of deserts, heat, and winds, as well as cultural information on the ektus.",
+  },
+  {
+    name: "Gaudimm",
+    blurb:
+      "A chemical language of subtle twitches and soft clicks used by the gau. Though the structure and grammar are surprisingly easy to learn, pheromonal bursts are required to communicate complex concepts. Lore might include detailed knowledge of caves, tunnels, and the progression of rot, as well as cultural information on the gau.",
+  },
+  {
+    name: "Knock",
+    blurb:
+      "The hissing, chittering voice of the tzelicrae hive-minds. Many of the sounds are impossible for a single mouth to form, so most outsiders that learn Knock speak a simplified form, relying partly on gesture. Lore might include a deeper knowledge of insects and arachnid habits, as well as cultural information on the tzelicrae.",
+  },
+  {
+    name: "Brasstongue",
+    blurb:
+      "A clipped and precise language used by experienced traders, adapted from the punch-cards of ancient ordinators. Knowledge of Brasstongue is likely to garner respect among merchants and grifters. Lore might include cargo routes and tales of great sales or negotiation tactics.",
+  },
+  {
+    name: "Raka Spit",
+    blurb:
+      "The rapid patter of hunting-families and leviathaneers, known for its incredible depth of descriptive words. Lore might include facts about the beasts and birds of the waves, and great conquests of hunters across the wilds.",
+  },
+  {
+    name: "Lyre-Bite",
+    blurb:
+      "The lilting tongue of poets and songwriters across the wild waves, well-suited to performances and long-form poetry. Lore might include ancient pre-verdant songs and a deeper understanding of whispers.",
+  },
+  {
+    name: "Old Hand",
+    blurb:
+      "An entirely gesture-based language with no spoken component, perfect for those naturally quiet types. Old Hand has existed since before the Verdancy, and has changed remarkably little despite the passage of years. Lore might include expansive descriptions of long-lost relics.",
+  },
+  {
+    name: "Signalling",
+    blurb:
+      "An unspoken code broadcast with flags, flares, or signal-lamps, often known by experienced travellers, military types and smugglers. Relies partly on colour. Lore might include information on various ships, weather types, and seagoing hazards.",
+  },
+  {
+    name: "Highvin",
+    blurb:
+      "Exceedingly difficult to master, Highvin is a primarily written language often found on old pre-verdant ruins. Usually understood by scholars, antiquarians, and treasure-hunters. Lore might include details of the pre-verdant times, though always fragmentary.",
+  },
+];
