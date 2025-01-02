f60ab24bc3618f15d39790ea2cf16c58b8ff3be9
PaulanEdu <132430937+PaulanEdu@users.noreply.github.com>
2025-01-02 03:42:48 -0500

    Update getElements.js
    Added declarations for drives and mires for all three playbooks

--- subscripts/getElements.js
+++ subscripts/getElements.js
@@ -16,11 +16,15 @@ const BLOODLINE_SKILL_BTNS = document.getElementById("Bloodline-skill-btns");
 const BLOODLINE_RESOURCE_BTNS = document.getElementById(
   "Bloodline-resource-btns"
 );
+const BLOODLINE_DRIVE_BTNS = document.getElementById("Bloodline-drive-btns");
+const BLOODLINE_MIRE_BTNS = document.getElementById("Bloodline-mire-btns");
 const BLOODLINE_OPTION_DIVS = [
   BLOODLINE_EDGE_BTNS,
   BLOODLINE_ASPECT_BTNS,
   BLOODLINE_SKILL_BTNS,
   BLOODLINE_RESOURCE_BTNS,
+  BLOODLINE_DRIVE_BTNS,
+  BLOODLINE_MIRE_BTNS,
 ];
 
 const ORIGIN_OPTIONS = document.getElementById("Origin-options");
@@ -28,11 +32,15 @@ const ORIGIN_EDGE_BTNS = document.getElementById("Origin-edge-btns");
 const ORIGIN_ASPECT_BTNS = document.getElementById("Origin-aspect-btns");
 const ORIGIN_SKILL_BTNS = document.getElementById("Origin-skill-btns");
 const ORIGIN_RESOURCE_BTNS = document.getElementById("Origin-resource-btns");
+const ORIGIN_DRIVE_BTNS = document.getElementById("Origin-drive-btns");
+const ORIGIN_MIRE_BTNS = document.getElementById("Origin-mire-btns");
 const ORIGIN_OPTION_DIVS = [
   ORIGIN_EDGE_BTNS,
   ORIGIN_ASPECT_BTNS,
   ORIGIN_SKILL_BTNS,
   ORIGIN_RESOURCE_BTNS,
+  ORIGIN_DRIVE_BTNS,
+  ORIGIN_MIRE_BTNS,
 ];
 
 const POST_OPTIONS = document.getElementById("Post-options");
@@ -40,11 +48,15 @@ const POST_EDGE_BTNS = document.getElementById("Post-edge-btns");
 const POST_ASPECT_BTNS = document.getElementById("Post-aspect-btns");
 const POST_SKILL_BTNS = document.getElementById("Post-skill-btns");
 const POST_RESOURCE_BTNS = document.getElementById("Post-resource-btns");
+const POST_DRIVE_BTNS = document.getElementById("Post-drive-btns");
+const POST_MIRE_BTNS = document.getElementById("Post-mire-btns");
 const POST_OPTION_DIVS = [
   POST_EDGE_BTNS,
   POST_ASPECT_BTNS,
   POST_SKILL_BTNS,
   POST_RESOURCE_BTNS,
+  POST_DRIVE_BTNS,
+  POST_MIRE_BTNS,
 ];
 
 export const OPTIONS_OBJECT = {
@@ -93,14 +105,13 @@ export const updateDisplay = (displayObj, sheet, skillsArr) => {
   for (let pb in displayObj) {
     displayObj[pb].name.innerHTML = sheet[pb].name;
     displayObj[pb].edge.innerHTML = sheet[pb].edge;
-    let skillsText = "";
 
+    let skillsText = "";
     skillsArr.forEach((skill) => {
       if (sheet[pb].skills.hasOwnProperty(skill)) {
         skillsText += `<br>${skill}: ${sheet[pb].skills[skill]}`;
       }
     });
-
     displayObj[pb].skills.innerHTML = skillsText;
   }
 };
